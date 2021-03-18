c60916251a25274dbbfb51aca85cf5a04b223001
Ken-muigai <kenmuigai980@gmail.com>
2021-03-18 06:23:21 +0300

    initial commits

---
+++ src/main/java/com/kenjoel/organised/OrganisedApplication.java
@@ -0,0 +1,13 @@
+package com.kenjoel.organised;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganisedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganisedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kenjoel/organised/OrganisedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenjoel.organised;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganisedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
